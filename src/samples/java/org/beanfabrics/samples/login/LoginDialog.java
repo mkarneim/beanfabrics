869d2384be6c798b1e31522ae1e1fce2b47ee422
max@gensthaler.de <max@gensthaler.de@d7cff0de-3b31-11de-b438-597f59cd7555>
2009-06-02 22:20:40 +0000

    suppress warning(serial)

--- src/samples/java/org/beanfabrics/samples/login/LoginDialog.java
+++ src/samples/java/org/beanfabrics/samples/login/LoginDialog.java
@@ -9,8 +9,8 @@ import org.beanfabrics.swing.BnButton;
 import org.beanfabrics.swing.BnPasswordField;
 import org.beanfabrics.swing.BnTextField;
 
+@SuppressWarnings("serial")
 public class LoginDialog extends JDialog implements View<LoginPM> {
-
 	private ModelProvider localModelProvider = new ModelProvider(); // @wb:location=8,202
 
 	private BnTextField tfUsername = new BnTextField();
@@ -18,7 +18,7 @@ public class LoginDialog extends JDialog implements View<LoginPM> {
 	private BnButton btnLogin = new BnButton();
 	private JLabel passwordLabel = new JLabel("Password");
 	private JLabel usernameLabel = new JLabel("Username");
-	
+
 	/**
 	 * Create the dialog
 	 */
@@ -29,26 +29,26 @@ public class LoginDialog extends JDialog implements View<LoginPM> {
 		setBounds(100, 100, 252, 174);
 		usernameLabel.setBounds(36, 23, 68, 15);
 		passwordLabel.setBounds(36, 49, 68, 15);
-		
+
 		tfUsername.setBounds(110, 21, 100, 20);
 		tfUsername.setModelProvider(localModelProvider);
 		tfUsername.setPath(new org.beanfabrics.Path("this.username"));
-		
+
 		tfPassword.setBounds(110, 47, 100, 20);
 		tfPassword.setModelProvider(localModelProvider);
 		tfPassword.setPath(new org.beanfabrics.Path("this.password"));
-		
+
 		btnLogin.setBounds(110, 91, 100, 25);
 		btnLogin.setModelProvider(localModelProvider);
 		btnLogin.setPath(new org.beanfabrics.Path("this.login"));
 		btnLogin.setText("Login");
-		
+
 		getContentPane().add(usernameLabel);
 		getContentPane().add(passwordLabel);
 		getContentPane().add(tfUsername);
 		getContentPane().add(tfPassword);
 		getContentPane().add(btnLogin);
-		
+
 		getRootPane().setDefaultButton(btnLogin);
 	}
 
@@ -60,6 +60,6 @@ public class LoginDialog extends JDialog implements View<LoginPM> {
 		localModelProvider.setPresentationModel(model);
 	}
 
-	
+
 
 }
