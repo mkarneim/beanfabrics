bbb20396e65f82880133ec26a954b8b56da552a0
mkarneim <mkarneim@d7cff0de-3b31-11de-b438-597f59cd7555>
2010-08-30 14:24:23 +0000

    Added BnCalendarChooserBeanInfo

---
+++ beanfabrics-swing-goodies/src/main/design/org/beanfabrics/swing/goodies/calendar/BnCalendarChooserBeanInfo.java
@@ -0,0 +1,35 @@
+/*
+ * Beanfabrics Framework Copyright (C) 2010 by Michael Karneim, beanfabrics.org
+ * Use is subject to license terms. See license.txt.
+ */
+package org.beanfabrics.swing.goodies.calendar;
+
+import java.beans.BeanInfo;
+
+import org.beanfabrics.swing.ModelSubscriberBeanInfo;
+
+/**
+ * @author Michael Karneim
+ */
+public class BnCalendarChooserBeanInfo extends ModelSubscriberBeanInfo {
+    @Override
+    protected Class getBeanClass() {
+        return BnCalendarChooser.class;
+    }
+
+    @Override
+    protected boolean isPathBound() {
+        return false;
+    }
+
+    @Override
+    public java.awt.Image getIcon(int iconKind) {
+        switch (iconKind) {
+            case BeanInfo.ICON_COLOR_16x16:
+                // TODO create an icon
+                return loadImage("bn_calendarchooser_obj16.png");
+            default:
+                return null;
+        }
+    }
+}
