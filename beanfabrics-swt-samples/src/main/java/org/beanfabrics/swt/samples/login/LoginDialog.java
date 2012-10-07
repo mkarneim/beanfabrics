8071077b153178e6cfc8e9f9b6fc6ef8797a1dbe
max@gensthaler.de <max@gensthaler.de@d7cff0de-3b31-11de-b438-597f59cd7555>
2012-10-07 19:39:21 +0000

    LoginDialog: removing @Override at implemented interface methods (java 1.6 feature)

--- beanfabrics-swt-samples/src/main/java/org/beanfabrics/swt/samples/login/LoginDialog.java
+++ beanfabrics-swt-samples/src/main/java/org/beanfabrics/swt/samples/login/LoginDialog.java
@@ -5,21 +5,21 @@ import org.beanfabrics.Path;
 import org.beanfabrics.View;
 import org.beanfabrics.swt.Decorator;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 /**
  * The {@link LoginDialog} is a dialog for entering user credentials.
- * 
+ *
  * @author Michael Karneim
  */
 public class LoginDialog extends Dialog implements View<LoginPM> {
-    private ModelProvider modelProvider = new ModelProvider();
+    private final ModelProvider modelProvider = new ModelProvider();
     protected Object result;
     protected Shell shlLogin;
     private Text tfUsername;
@@ -59,24 +59,24 @@ public class LoginDialog extends Dialog implements View<LoginPM> {
      * Create contents of the dialog.
      */
     private void createContents() {
-        shlLogin = new Shell(getParent(), getStyle());        
+        shlLogin = new Shell(getParent(), getStyle());
         shlLogin.setSize(319, 166);
         shlLogin.setText("Login");
-        
+
         tfUsername = new Text(shlLogin, SWT.BORDER);
         tfUsername.setBounds(65, 13, 150, 19);
-        
+
         Label lblUsername = new Label(shlLogin, SWT.NONE);
         lblUsername.setBounds(10, 16, 49, 13);
         lblUsername.setText("Username");
-        
+
         Label lblPassword = new Label(shlLogin, SWT.NONE);
         lblPassword.setBounds(10, 41, 49, 13);
         lblPassword.setText("Password");
-        
+
         tfPassword = new Text(shlLogin, SWT.BORDER | SWT.PASSWORD);
         tfPassword.setBounds(65, 38, 150, 19);
-        
+
         btnLogin = new Button(shlLogin, SWT.NONE);
         btnLogin.addSelectionListener(new SelectionAdapter() {
             @Override
@@ -85,8 +85,8 @@ public class LoginDialog extends Dialog implements View<LoginPM> {
             }
         });
         btnLogin.setBounds(10, 108, 68, 23);
-        btnLogin.setText("Login");        
-        
+        btnLogin.setText("Login");
+
         btnCancel = new Button(shlLogin, SWT.NONE);
         btnCancel.addSelectionListener(new SelectionAdapter() {
             @Override
@@ -97,21 +97,19 @@ public class LoginDialog extends Dialog implements View<LoginPM> {
         btnCancel.setBounds(235, 108, 68, 23);
         btnCancel.setText("Cancel");
     }
-    
+
     private void bindContents() {
-        Decorator deco = new Decorator(modelProvider);        
+        Decorator deco = new Decorator(modelProvider);
         deco.decorateText(tfUsername, new Path("username"));
         deco.decorateText(tfPassword, new Path("password"));
         deco.decoratePushButton(btnLogin, new Path("login"));
     }
 
-    @Override
     public LoginPM getPresentationModel() {
         return modelProvider.getPresentationModel();
     }
 
-    @Override
     public void setPresentationModel(LoginPM pModel) {
-        modelProvider.setPresentationModel(pModel);        
+        modelProvider.setPresentationModel(pModel);
     }
 }
